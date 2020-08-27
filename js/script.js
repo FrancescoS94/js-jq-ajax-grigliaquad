403d547de2845fff6d825c157f0551cf83799c25
FrancescoS <francesco.salerno2294@gmail.com>
2020-08-27 17:11:06 +0200

    End

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Generare una griglia 6x6 (36 boxes),
+// ad ogni click parte una richiesta AJAX che prende
+// un numero random da 1 a 9 (scegliere API opportuna).
+// Se è <= 5 il quadrato diventa giallo, se è > di 5
+// il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+
+
+$( document ).ready(function() {
+    //Ciclo for per clonazione quadrati
+    for (var i = 0; i < 36; i++) {
+        $('.center').append('<div class="box"></div>');
+    }
+
+    $(".box").on("click", function() {
+        var testo = $(this);
+
+        // Ad ogni click parte una richiesta AJAX
+        // che prende un numero random da 1 a 9.
+        $.ajax(
+        {
+         "url": "https://flynn.boolean.careers/exercises/api/random/int",
+         "method": "GET",
+         "success": function (risposta) {
+            testo.text(risposta.response)
+
+            // Se è <= 5 il quadrato diventa giallo, se è > di 5
+            // il quadrato diventa verde.
+            if (risposta.response <= 5) {
+                testo.addClass("yellow");
+            } else {
+                testo.addClass("green");
+            }
+         },
+         error: function () {
+         alert("E' avvenuto un errore.");
+         }
+        }
+        );
+    });
+});
